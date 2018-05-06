8ea98e7c6618ac3175b4ad67809dae9c93264ab8
Abiev Andrew <andreyka.abiev@gmail.com>
2018-05-06 18:56:31 +0300

    NewFolder

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+                            (function()
+                            {
+                                var div = document.getElementsByClassName('progress');
+                                style = div[0].style;
+                                if(div[0].textContent = "HTML")
+                                    style.width = "45%";
+                                
+                                style = div[1].style;
+                                if(div[1].textContent = "CSS")
+                                    style.width = "40%";
+                                
+                                style = div[2].style;
+                                if(div[2].textContent = "JS")
+                                    style.width = "30%";
+                                
+                                style = div[3].style;
+                                if(div[3].textContent = "JQuery")
+                                    style.width = "30%";
+                                
+                                style = div[4].style;
+                                if(div[4].textContent = "MS-Sql")
+                                    style.width = "50%";
+                                
+                                style = div[5].style;
+                                if(div[5].textContent = "C#/.NET")
+                                    style.width = "60%";
+                                
+                                style = div[6].style;
+                                if(div[6].textContent = "ADO.NET")
+                                    style.width = "20%";
+                            })();
